e14010c1e50372f609264cb03dd4551c5b597e18
vishal kanns <136556305+vishalkannsgit@users.noreply.github.com>
2023-09-04 21:35:52 +0530

    Add files via upload

---
+++ APP4.py
@@ -0,0 +1,15 @@
+
+name = "vishal"
+address = "Namakkal"
+total_subjects = 6
+sub1 = 70
+sub2 = 67
+sub3 = 87
+sub4 = 69
+sub5 = 90
+sub6 = 87
+total = sub1 + sub2 + sub3 + sub4 + sub5
+
+average = (total / total_subjects)
+
+print ( " i m ", name, "i m from ", address, "My 12th marks total is : ",total , " My average mark is : ", average)
---
+++ VSapp2.py
@@ -0,0 +1,8 @@
+x = 30
+print(x)
+print(x+x)
+print(x*x)
+print(x-x)
+print(x%x)
+print(x/x)
+print("My first program")
